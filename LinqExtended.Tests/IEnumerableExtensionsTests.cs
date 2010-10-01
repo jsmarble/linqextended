dd61b2c5616d064e448c1a4619c16b507aadea46
Joshua Marble <jsmarble@gmail.com>
2010-10-01 09:09:42 -0500

    Initial project with extensions and tests.

---
+++ LinqExtended.Tests/IEnumerableExtensionsTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace LinqExtended.Tests
+{
+    public class IEnumerableExtensionsTests
+    {
+        [Fact]
+        public void ContainsAtLeast_Checking_Less_Than_Actual_Returns_False()
+        {
+            int count = 10;
+            var numbers = Enumerable.Range(1, count - 1);
+            bool expected = false;
+            bool actual = numbers.ContainsAtLeast(count);
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void ContainsAtLeast_Checking_More_Than_Actual_Returns_True()
+        {
+            int count = 10;
+            var numbers = Enumerable.Range(1, count + 1);
+            bool expected = true;
+            bool actual = numbers.ContainsAtLeast(count);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ContainsAtLeast_Checking_Exactly_Actual_Returns_True()
+        {
+            int count = 10;
+            var numbers = Enumerable.Range(1, count);
+            bool expected = true;
+            bool actual = numbers.ContainsAtLeast(count);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ContainsAtMost_Checking_Less_Than_Actual_Returns_True()
+        {
+            int count = 10;
+            var numbers = Enumerable.Range(1, count - 1);
+            bool expected = true;
+            bool actual = numbers.ContainsAtMost(count);
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void ContainsAtMost_Checking_More_Than_Actual_Returns_False()
+        {
+            int count = 10;
+            var numbers = Enumerable.Range(1, count + 1);
+            bool expected = false;
+            bool actual = numbers.ContainsAtMost(count);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ContainsAtMost_Checking_Exactly_Actual_Returns_True()
+        {
+            int count = 10;
+            var numbers = Enumerable.Range(1, count);
+            bool expected = true;
+            bool actual = numbers.ContainsAtMost(count);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ LinqExtended/IEnumerableExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinqExtended
+{
+    public static class IEnumerableExtensions
+    {
+        public static bool ContainsAtLeast<T>(this IEnumerable<T> source, int count)
+        {
+            if (count < 0)
+                throw new ArgumentOutOfRangeException("count");
+
+            int actualCount = 0;
+            IEnumerator<T> enumerator = source.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                actualCount++;
+                if (actualCount >= count)
+                    return true;
+            }
+            return false;
+        }
+
+        public static bool ContainsAtMost<T>(this IEnumerable<T> source, int count)
+        {
+            if (count < 0)
+                throw new ArgumentOutOfRangeException("count");
+
+            int actualCount = 0;
+            IEnumerator<T> enumerator = source.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                actualCount++;
+                if (actualCount > count)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
